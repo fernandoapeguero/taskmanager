ef6b88e1e2aadc9b56c3d062cc8029a1f6972b69
fernandoapeguero <fernandoapeguero@gmail.com>
2022-05-09 16:29:50 -0400

    Implemented Input field use of state values

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
-interface AppProps {
-  name: string;
-}
+import InputField from "./components/InputField";
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+
   return (
     <div className="App">
       <span className="heading">Task Manager</span>
+
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import "./style.css";
 
-const InputField: React.FC = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+  console.log(todo);
   return (
     <form className="input">
-      <input type="input" placeholder="Enter a task" className="input__box" />
+      <input
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        type="input"
+        placeholder="Enter a task"
+        className="input__box"
+      />
       <button className="input__submit" type="submit">
         Go
       </button>
